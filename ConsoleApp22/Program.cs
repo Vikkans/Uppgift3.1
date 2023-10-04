12e970ff14ee8bf6217bf2555714ecd807b8026f
Vikkans <Jakobsenviktor6@gmail.com>
2023-10-04 10:20:44 +0200

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            string x = Console.ReadLine();
+            int ålder = int.Parse(x);
+
+            switch(ålder)
+            {
+                case 16:
+                    {
+                        Console.WriteLine("Du får vara med");
+                          break;
+                    }
+
+                case 17:
+                    {
+                        Console.WriteLine("Du får vara med");
+                          break;
+                    }
+
+
+                case 18:
+                    {
+                        Console.WriteLine("Du får vara med");
+                          break;
+                    }
+
+
+                case 19:
+                    {
+                        Console.WriteLine("Du får vara med");
+                          break;
+                    }
+
+
+                default:
+                    {
+                        Console.WriteLine("Du får inte vara med");
+                        break;
+                    }
+
+
+                
+
+
+            }
+
+            Console.ReadLine();
+        }
+       
+    }
+
+
+}
